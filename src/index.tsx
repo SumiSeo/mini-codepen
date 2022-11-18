43e88e2f2221956decfcd26144ec2b677daa8c48
Sumi <qkobr94@gmail.com>
2022-11-18 18:23:45 +0900

    initialize es-build

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,37 @@
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  return <h1>hi</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 root.render(<App />);
